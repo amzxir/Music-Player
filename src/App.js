d6941cee6344883b454b9a6e84dfad2046066e12
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2022-09-25 13:40:29 +0330

    nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+import Navbar from "./nav/nav";
+
+const Container = styled.div({
+  padding:'0px 20px 0px 20px'
+})
 
 const App = () => {
   return ( 
     <React.Fragment>
-      
+      <Navbar/>
+      <Container>
+
+      </Container>
     </React.Fragment>
   );
 }
---
+++ src/nav/nav.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Nav = styled.nav({
+    padding:'15px 20px',
+    boxShadow:'0 .125rem .25rem rgba(0,0,0,.075)',
+    position:'sticky',
+})
+
+const Navbar = () => {
+    return ( 
+        <Nav>
+            ssjj
+        </Nav>
+    );
+}
+ 
+export default Navbar;
